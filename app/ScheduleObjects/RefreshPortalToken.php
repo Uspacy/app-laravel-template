39311b6ea9e6db9c906814ccca65ce36b4673a13
Anton <medoc1948@gmail.com>
2024-10-02 19:27:51 +0300

    implemented portals deactivation

--- app/ScheduleObjects/RefreshPortalToken.php
+++ app/ScheduleObjects/RefreshPortalToken.php
@@ -4,6 +4,7 @@ namespace App\ScheduleObjects;
 
 use App\Models\Portal;
 use App\Services\External\UserService as ExternalUserService;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 
@@ -16,19 +17,34 @@ class RefreshPortalToken
         $filterTime = Carbon::now()->timestamp + config('portal.rerfesh_token_schedule');
 
         $portals = Portal::where('expiry_date', '<=', $filterTime)->get();
-        
+
+        $appName = config('api.code');
+
         foreach ($portals as $portal) {
+            $portalDomain = $portal->domain;
+
+            if (!$portal->is_active) {
+                continue;
+            }
+
             try {
                 $tokenData = $externalUserService->refreshToken($portal);
 
                 $portal->token = $tokenData->jwt;
                 $portal->refresh_token = $tokenData->refreshToken;
                 $portal->expiry_date = Carbon::now()->timestamp + $tokenData->expireInSeconds;
+                $portal->failed_token_refresh_attempts = 0;
                 $portal->save();
-            }
-            catch (\Throwable $th) {
-                $appName = config('api.code');
-                $portalDomain = $portal->domain;
+            }catch (RequestException $requestException) {
+                $portal->failed_token_refresh_attempts++;
+                if ($portal->failed_token_refresh_attempts >= 5) {
+                    $portal->is_active = false;
+                }
+
+                $portal->save();
+
+                Log::warning("App@{$appName} refresh token for portal {$portalDomain}:" . $requestException->getMessage());
+            } catch (\Throwable $th) {
                 Log::error("App@{$appName} refresh token for portal {$portalDomain}:" . $th->getMessage());
             }
         }
